4d856e0bcd86a513ab41fc4a14156e2546feb043
Sahil Garg <Sahil.Garg@evryindia.in>
2024-12-06 00:57:17 +0530

    added and modified the files

---
+++ scripts/cleanup_old_files.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# cleanup_old_files.sh
+
+# Example cleanup task: remove old files
+echo "Cleaning up old application files..."
+rm -rf /home/ec2-user/myapp/*
+
+# You can add any other pre-installation tasks here.
