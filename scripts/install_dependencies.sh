794565e26876cacd62615986afd3f686c6f10dde
Sahil Garg <Sahil.Garg@evryindia.in>
2024-12-06 23:54:10 +0530

    change ec2 direcroy

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -6,9 +6,9 @@ sudo apt-get update
 sudo apt-get install -y python3 python3-pip python3-venv
 
 # Create a virtual environment
-# cd /home/ubuntu/myapp
-# python3 -m venv venv
+cd /home/ec2-user/myapp
+python3 -m venv venv
 
 # Activate the virtual environment and install dependencies
-# source venv/bin/activate
+source venv/bin/activate
 pip install -r requirements.txt
