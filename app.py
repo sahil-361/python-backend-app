31289098e6c0436e2f80ba1a9de05e900a332c34
sahil <sahil.garg1890@gmail.com>
2024-12-08 16:50:53 +0530

    dev

--- app.py
+++ app.py
@@ -6,6 +6,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return "Hello, World test Dev Branch..."
+    return "Hello, World test Dev Branch... without hooks"
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
