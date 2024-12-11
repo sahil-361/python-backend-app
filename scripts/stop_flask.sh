697f667d4c2f9b84f9cdff91d07a9d48d1120c29
sahil <sahil.garg1890@gmail.com>
2024-12-11 23:35:22 +0530

    fi

--- scripts/stop_flask.sh
+++ scripts/stop_flask.sh
@@ -4,10 +4,4 @@
 # Find the process ID (PID) of the running Flask app and kill it
 FLASK_PID=$(ps aux | grep 'python app.py' | grep -v grep | awk '{print $2}')
 
-if [ -z "$FLASK_PID" ]; then
-    echo "Flask app is not running."
-else
-    echo "Stopping Flask app with PID: $FLASK_PID"
-    kill $FLASK_PID
-    echo "Flask app stopped."
-fi
+kill $FLASK_PID
