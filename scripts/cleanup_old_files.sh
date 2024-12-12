7a65d655ce48ecba22acf14793aaf1415f3b6a4a
sahil <sahil.garg1890@gmail.com>
2024-12-12 12:10:11 +0530

    old

--- scripts/cleanup_old_files.sh
+++ scripts/cleanup_old_files.sh
@@ -3,6 +3,6 @@
 
 # Example cleanup task: remove old files
 echo "Cleaning up old application files..."
-rm -rf /home/ec2-user/myapp/*
+rm -rf /home/ec2-user/myapp/.* 2>/dev/null
 
 # You can add any other pre-installation tasks here.
